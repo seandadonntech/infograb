0b54d510faecfdf89b5ae0334f0af7cbfcac61b9
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2023-02-21 15:33:06 -0500

    Update infograb.py

--- infograb.py
+++ infograb.py
@@ -1,27 +1,32 @@
-
 import os
 import socket
+import dns.reversename
 from time import *
-
-from colorama import  init, Back, Fore, Style
-init(convert=True)
-cammand = input(Fore.GREEN +"[+]ENTER THE COMMAND YOU TO EXECUTE\n")
+from netaddr import *
+from colorama import Fore, Back, Style
+cammand = input(Fore.GREEN +"[+] ENTER THE COMMAND YOU TO EXECUTE:\n")
 if cammand == "ip":
  hostname = socket.gethostname()
  IPAddr = socket.gethostbyname(hostname)
 print(f"The host name of this computer/machine is  {hostname}"  )
 print(f"Your Computer IP Address is: {IPAddr} " )
+ip = IPAddress(f'{IPAddr}').ipv6()
+print(f"the Target Machine is ipv6 is{ip}")
+domain = dns.reversename.from_address(f"{IPAddr}")
+print(domain)
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-question = input("[+] press w to put information a file\n ")
+question = input("[+] press w to put information a file:\n ")
 if question == "w":
 
- f = open("info.txt", "a")
+ f = open("info.txt", "a") #appends the fle/ file name is changeable
+ print("file was made")
 
- f.write(f"\n Targeted machine name is {hostname},\n Targeted machine ipv4 is {IPAddr}")
+ f.write(f"\n Targeted machine name is {hostname},\n Targeted machine ipv4 is {IPAddr}, \n Targeted machine ipv6 is {ip}, \n The reverse dns name is {domain},\n  on Targeted machine") # write the date file
  f.closed
 else:
- print("file not made")
+ print("file was not made")
 
-sleep(150)
-#more thing will be added
+
+
+
